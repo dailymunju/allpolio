cfef40a99d9fed895c6fc7bdb0095d985816ce96
dailymunju <moonjucho32@gmail.com>
2023-12-25 06:00:40 +0900

    dist 다시

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-/* export default defineConfig({
-  plugins: [react()],
-}) */
 export default defineConfig({
   plugins: [react()],
     build: {
-        chunkSizeWarningLimit: 100000000
-    },
+      chunkSizeWarningLimit: 1600
+  }
 })
+
